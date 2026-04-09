8a8d388362fc0eb596d06abc2c5762410997b19d
Zane Allen <zhma14@gmail.com>
2026-04-09 16:24:38 -0400

    Added sign up feature

--- JavaScript/loginscript.js
+++ JavaScript/loginscript.js
@@ -1,4 +1,8 @@
+const BtnLogin = document.getElementById("BtnLogin");
+const BtnSignUp = document.getElementById("BtnSignUp");
+BtnSignUp.addEventListener("click", SignUp);
 BtnLogin.addEventListener("click", Login);
+const userList = [{username:"admin",password:"password"},{username:"admin2",password:"password2"}];
 
 function Login(){
     //Collect username and password
@@ -6,7 +10,6 @@ function Login(){
     const userPass = document.getElementById("userPass");
     const readName = userName.value;
     const readPass = userPass.value;
-    const userList = [{username:"admin",password:"password"},{username:"admin2",password:"password2"}];
     
     //Make sure there is something in the input
     if (readName !== "" && readPass !== ""){
@@ -31,4 +34,32 @@ function Login(){
         alert("Please enter anything");
     }
 }
+
+function SignUp(){
+    const userName = document.getElementById("userName");
+    const userPass = document.getElementById("userPass");
+    const readName = userName.value;
+    const readPass = userPass.value;
+    //Make sure there is something in the input
+    if (readName !== "" && readPass !== ""){
+            console.log("step one")
+            //Check if user already exists
+            const userExists = userList.some(user => user.username === readName);
+            if(!userExists){
+                // If user doesn't exist, create new user
+                userList.push({username: readName, password: readPass});
+                userName.value = "";
+                userPass.value = "";
+                alert("Sign up successful!");
+            }else{
+                userName.value = "";
+                userPass.value = "";
+                alert("User already exists!");
+            }
+        }else{
+            userName.value = "";
+            userPass.value = "";
+            alert("Please enter anything");
+        }
+    }
 
